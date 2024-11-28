72a7b94a040f8037f61d0b3c4346da9db2407911
Igor Aralov <igor.aralov@rambler.ru>
2024-11-28 21:18:06 +0300

    Add StatefullModalForm

--- src/shared/modal-form/ModalForm.tsx
+++ src/shared/modal-form/ModalForm.tsx
@@ -1,17 +1,20 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, MouseEvent } from 'react';
 import cn from 'clsx';
 import s from './ModalForm.module.scss';
 
 type ModalFormProps = {
   visible: boolean;
   children: ReactNode;
+  onClose?: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
-export const ModalForm = ({ visible = true, children }: ModalFormProps) => {
+export const ModalForm = ({ visible = true, onClose, children }: ModalFormProps) => {
   return (
     <div id="myModal" className={cn(s.modal, { [s['modal-hide']]: !visible })}>
       <div className={s['modal-content']}>
-        <span className={s.close}>&times;</span>
+        <span className={s.close} onClick={onClose}>
+          &times;
+        </span>
         {children}
       </div>
     </div>
