d236ab1a7b35c614dc5674643fa2dd1cc0349aa1
Igor Aralov <igor.aralov@rambler.ru>
2024-12-04 21:13:40 +0300

    Fix modal form

--- src/shared/modal-form/ModalForm.tsx
+++ src/shared/modal-form/ModalForm.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode, MouseEvent } from 'react';
-import cn from 'clsx';
 import s from './ModalForm.module.scss';
 
 type ModalFormProps = {
@@ -8,9 +7,9 @@ type ModalFormProps = {
   onClose?: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
-export const ModalForm = ({ visible = true, onClose, children }: ModalFormProps) => {
-  return (
-    <div id="myModal" className={cn(s.modal, { [s['modal-hide']]: !visible })}>
+export const ModalForm = ({ visible = true, onClose, children }: ModalFormProps) =>
+  !!visible && (
+    <div className={s.modal}>
       <div className={s['modal-content']}>
         <span className={s.close} onClick={onClose}>
           &times;
@@ -19,4 +18,3 @@ export const ModalForm = ({ visible = true, onClose, children }: ModalFormProps)
       </div>
     </div>
   );
-};
